4cc58d13f72da69bdfed13cc318d824431748812
Andrés A. Cortés <andresbeto315@gmail.com>
2023-07-08 12:06:05 -0500

    Agregar funcionalidad controladores

--- API/Controllers/CategoryController.cs
+++ API/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Base;
+using Domain.Contracts.Application;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,5 +8,47 @@ namespace API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this._categoryService = categoryService;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var data = this._categoryService.GetAll();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse<string>
+                {
+                    Code = System.Net.HttpStatusCode.BadRequest,
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var data = this._categoryService.GetById(id);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse<string>
+                {
+                    Code = System.Net.HttpStatusCode.BadRequest,
+                    Message = ex.Message
+                });
+            }
+        }
     }
 }
--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Domain.Base;
+using Domain.Contracts.Application;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -7,5 +9,65 @@ namespace API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        [HttpGet("all")]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var data = this._productService.GetAll();
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse<string>
+                {
+                    Code = System.Net.HttpStatusCode.BadRequest,
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                var data = this._productService.GetById(id);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse<string>
+                {
+                    Code = System.Net.HttpStatusCode.BadRequest,
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("code")]
+        public IActionResult GetByCode(string code)
+        {
+            try
+            {
+                var data = this._productService.GetByCode(code);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new BaseResponse<string>
+                {
+                    Code = System.Net.HttpStatusCode.BadRequest,
+                    Message = ex.Message
+                });
+            }
+        }
     }
 }

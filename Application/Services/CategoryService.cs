fd9eec4fbae65a407ba95700cc2ad01bf2f9fd3f
Andrés A. Cortés <andresbeto315@gmail.com>
2023-07-08 11:58:57 -0500

    Agregar funcionalidad de servicios

--- Application/Services/CategoryService.cs
+++ Application/Services/CategoryService.cs
@@ -1,8 +1,59 @@
-﻿using Domain.Contracts.Application;
+﻿using Domain.Base;
+using Domain.Contracts.Application;
+using Domain.Contracts.Persistance;
+using Domain.Dtos;
+using Domain.Entities;
 
 namespace Application.Services
 {
     public class CategoryService : ICategoryService
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
+        private static CategoryDto Mapper(CategoryEntity category)
+        {
+            return new CategoryDto
+            {
+                Id = category.idcategoria,
+                Description = category.descripcion,
+                Name = category.nombre,
+                State = category.estado
+            };
+        }
+
+        public async Task<BaseResponse<List<CategoryDto>>> GetAll()
+        {
+            var categories = await this._categoryRepository.GetAll();
+            var data = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                data.Add(Mapper(category));
+            }
+            return new BaseResponse<List<CategoryDto>>
+            {
+                Data = data,
+                Code = System.Net.HttpStatusCode.OK,
+                Message = "Consulta satisfactoria"
+            };
+        }
+
+        public async Task<BaseResponse<CategoryDto>> GetById(int id)
+        {
+            if (id < 0) throw new Exception("El id de búsqueda no es valido");
+
+            var category = await this._categoryRepository.GetById(id);
+            var data = Mapper(category);
+            return new BaseResponse<CategoryDto>
+            {
+                Data = data,
+                Code = System.Net.HttpStatusCode.OK,
+                Message = "Consulta satisfactoria"
+            };
+        }
     }
 }
--- Application/Services/ProductService.cs
+++ Application/Services/ProductService.cs
@@ -1,8 +1,78 @@
-﻿using Domain.Contracts.Application;
+﻿using Domain.Base;
+using Domain.Contracts.Application;
+using Domain.Contracts.Persistance;
+using Domain.Dtos;
+using Domain.Entities;
 
 namespace Application.Services
 {
     public class ProductService : IProductService
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            this._productRepository = productRepository;
+        }
+
+        private static ProductDto Mapper(ProductEntity product)
+        {
+            return new ProductDto
+            {
+                Id = product.idproducto,
+                Category = product.category,
+                Code = product.codigo,
+                Description = product.descripcion,
+                Image = product.imagen,
+                Name = product.nombre,
+                Price = product.precio_venta,
+                State = product.estado,
+                Stock = product.stock
+            };
+        }
+
+        public async Task<BaseResponse<List<ProductDto>>> GetAll()
+        {
+            var products = await this._productRepository.GetAll();
+            var data = new List<ProductDto>();
+            foreach (var product in products)
+            {
+                data.Add(Mapper(product));
+            }
+            return new BaseResponse<List<ProductDto>>
+            {
+                Data = data,
+                Code = System.Net.HttpStatusCode.OK,
+                Message = "Consulta satisfactoria"
+            };
+        }
+
+        public async Task<BaseResponse<ProductDto>> GetByCode(string code)
+        {
+            if (string.IsNullOrEmpty(code)) throw new Exception("El codigo de búsqueda no es valido");
+
+            var product = await this._productRepository.GetByCode(code);
+            var data = Mapper(product);
+            return new BaseResponse<ProductDto>
+            {
+                Data = data,
+                Code = System.Net.HttpStatusCode.OK,
+                Message = "Consulta satisfactoria"
+            };
+        }
+
+        public async Task<BaseResponse<ProductDto>> GetById(int id)
+        {
+            if (id < 0) throw new Exception("El id de búsqueda no es valido");
+
+            var product = await this._productRepository.GetById(id);
+            var data = Mapper(product);
+            return new BaseResponse<ProductDto>
+            {
+                Data = data,
+                Code = System.Net.HttpStatusCode.OK,
+                Message = "Consulta satisfactoria"
+            };
+        }
     }
 }
